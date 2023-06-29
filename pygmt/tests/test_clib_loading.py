d4c2b635b673cc66a9fad84077205d6cd6f4cc77
Dongdong Tian <seisman.info@gmail.com>
2023-06-29 16:16:37 +0800

    One more debug

--- pygmt/tests/test_clib_loading.py
+++ pygmt/tests/test_clib_loading.py
@@ -287,6 +287,8 @@ def test_clib_full_names_gmt_library_path_undefined_path_included(
         # Windows: find_library() searches the library in PATH, so one more
         npath = 2 if sys.platform == "win32" else 1
         print(sys.platform)
+        if sys.platform == "win32":
+            print(find_library("gmt.dll"))
         print(list(lib_fullpaths))
         print([gmt_lib_realpath] * npath + gmt_lib_names)
         assert list(lib_fullpaths) == [gmt_lib_realpath] * npath + gmt_lib_names
